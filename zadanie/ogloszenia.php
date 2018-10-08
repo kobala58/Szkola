1fde32968c2676fe7898735b605509dd42c41bd7
Bartłomiej Koba <34425167+kobala58@users.noreply.github.com>
2018-10-08 10:15:31 +0200

    Add files via upload

---
+++ zadanie/ogloszenia.php
@@ -0,0 +1,61 @@
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Poratal ogloszeniowy</title>
+  <link rel="stylesheet" href="styl1.css">
+  <script src="aktualizacja.js" ></script>
+<head>
+
+<body>
+  <div class="banner">
+    <h1>Portal Ogłoszeniowy</h1>
+  </div>
+
+  <div class="panel_l">
+    <h2>Kategorie ogłoszeń</h2>
+      <ol>
+        <li>Ksiażki</li>
+        <li>Muzyka</li>
+        <li>Filmy</li>
+      </ol>
+
+      <img src="ksiazki.jpg"/>
+
+      <table>
+        <tr>
+          <td>Liczba ogloszen</td>
+          <td>Cena ogloszenia</td>
+          <td>Bonus</td>
+        </tr>
+        <tr>
+          <td>1-10</td>
+          <td>1 PLN</td>
+          <td rowspan="3">Subskrypcja newslettera</br> to upust 0,20 PLN na</br> ogłosznie</td>
+        </tr>
+        <tr>
+          <td>11-50</td>
+          <td>0,80 PLN</td>
+
+        </tr>
+        <tr>
+          <td>51 i wiecej</td>
+          <td>0,60 PLN</td>
+
+        </tr>
+      </table>
+
+  </div>
+  <div class="panel_r">
+    <h2>Ogłoszenia kategorii książki</h2>
+  </div>
+  <footer>
+    Portal ogłoszeniowy opracował: Bartlomiej Koba
+    Ostatnia modyfikacja:
+
+			<script>
+			czas()
+			</script>
+  </footer>
+</body>
+
+</html>
